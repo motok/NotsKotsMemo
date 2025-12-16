aa4ff7bb4922234973d492649642c3efc5ef6f2f
moto kawasaki <moto@chunyan.local>
2025-12-16 11:27:22 +0900

    quick & dirty hack on listing wireguard (live) tunnels from wg show output.

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 個人的なメモなので、錯誤あるものとしてご覧ください。
 
+- 2025-12-16 [Wireguardの状態表示スクリプトをquick&dirty hackした](./wg_list.sh)
 - 2025-03-26 [今更ながらunboundでDNSSECを検証できるようにする](./unbound-DNSSEC/)
 - 2025-03-10 [HDD GELI化](./UFS_on_GELI/)
 - 2024-06-09 [Flet's もどき](./FletsLikeNW/FletsLikeNW.md)
---
+++ wg_list.sh
@@ -0,0 +1,69 @@
+#! /bin/sh
+
+# wg_list.sh
+#
+# parsing `wg show` output into tab-separated table.
+# use like `sudo wg show | ./wg_list.sh`.
+# remember that ever-down tunnel will now show.
+
+awk '
+
+function time2second (str) {
+	seconds = 0
+
+	p = match(str, /^ *[0-9]+ seconds? ago$/)
+	if (p > 0) {
+		split(str, a)
+		seconds +=  a[1]
+	}
+
+	p = match(str, /^ *[0-9]+ minutes?, [0-9]+ seconds? ago$/)
+	if (p > 0) {
+		split(str, a)
+		seconds +=  a[1] * 60 + a[3]
+	}
+
+	p = match(str, /^ *[0-9]+ hours?, [0-9]+ minutes?, [0-9]+ seconds? ago$/)
+	if (p > 0) {
+		split(str, a)
+		seconds +=  a[1] * 3600 + a[3] * 60 + a[5]
+	}
+
+	p = match(str, /^ *[0-9]+ days?, [0-9]+ hours?, [0-9]+ minutes?, [0-9]+ seconds? ago$/)
+	if (p > 0) {
+		split(str, a)
+		seconds +=  a[1] * 86400 + a[3] * 3600 + a[5] * 60 + a[7]
+	}
+
+	return seconds
+}
+
+BEGIN {
+	OFS = "\t"
+	print "#interface	listen_port	endpoint	allowed_ips	latest_handshake(sec)	lt_5min"
+}
+
+/^interface:/ { WG_IF = $2 }
+
+/^ *listening port:/ { WG_PORT = $3 }
+
+/^ *endpoint:/ { END_POINT = $2 }
+
+/^ *allowed ips:/ {
+	ALLOWED_IPS = ""
+	for (i=3; i<=NF; i++) {
+		ALLOWED_IPS = ALLOWED_IPS " " $i
+	}
+}
+
+/^ *latest handshake:/ {
+	LATEST_HS = ""
+	for (i=3; i<=NF; i++) {
+		LATEST_HS = LATEST_HS " " $i
+	}
+	
+	seconds = time2second(LATEST_HS)
+	print WG_IF, WG_PORT, END_POINT, ALLOWED_IPS, seconds, (seconds<300 ? "True" : "False")
+}
+
+' $*
